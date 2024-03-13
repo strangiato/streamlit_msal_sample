132860f7c0fddc36664fd08b7201eb90189d90a4
Trevor Royer <troyer@redhat.com>
2024-03-13 16:03:05 -0600

    add test app

--- app/dashboard.py
+++ app/dashboard.py
@@ -5,8 +5,8 @@ from msal_streamlit_authentication import msal_authentication
 
 value = msal_authentication(
     auth={
-        "clientId": "aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee",
-        "authority": "https://login.microsoftonline.com/aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee",
+        "clientId": "95d75130-08e9-4d56-935f-1cc063a81177",
+        "authority": "https://login.microsoftonline.com/8a9d6f8c-3d41-469e-ab2f-16738a0bb216",
         "redirectUri": "/",
         "postLogoutRedirectUri": "/"
     },
@@ -14,8 +14,8 @@ value = msal_authentication(
         "cacheLocation": "sessionStorage",
         "storeAuthStateInCookie": False
     },
-    login_request={
-        "scopes": ["aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee/.default"]
-    },
+    # login_request={
+    #     "scopes": ["aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee/.default"]
+    # },
     key=1)
 st.write("Received", value)
