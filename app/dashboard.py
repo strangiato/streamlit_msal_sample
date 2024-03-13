5f77ff4bc2c8d0cf0fd4307f1b4ce1935a51da6a
Trevor Royer <troyer@redhat.com>
2024-03-13 16:11:04 -0600

    set as env vars

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,12 +1,17 @@
+import os
+
 import streamlit as st
 
 from msal_streamlit_authentication import msal_authentication
 
+clientID = os.environ["CLIENTID"]
+tenantID = os.environ["TENANTID"]
+
 
 value = msal_authentication(
     auth={
-        "clientId": "95d75130-08e9-4d56-935f-1cc063a81177",
-        "authority": "https://login.microsoftonline.com/8a9d6f8c-3d41-469e-ab2f-16738a0bb216",
+        "clientId": clientID,
+        "authority": f"https://login.microsoftonline.com/{tenantID}",
         "redirectUri": "/",
         "postLogoutRedirectUri": "/"
     },
